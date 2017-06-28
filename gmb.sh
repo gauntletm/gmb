78b6d8f99015920bb0dbb3b2925ae57c58d85b3a
Gauntlet O. Manatee <spukspital@openmailbox.org>
2017-06-29 01:18:33 +0200

    replace backticks
    backticks are deprecated, so $() is used now.

--- gmb.sh
+++ gmb.sh
@@ -14,7 +14,7 @@ fi
 
 if [ ! -f $blogdir/index.html ]
 then
-  postdate=`date --utc +%Y-%m-%d`
+  postdate=$(date --utc +%Y-%m-%d)
   echo -e '<!DOCTYPE html>\n<html lang='"$bloglang"'>\n<head>\n<title>'"$blogtitle"'</title>\n<meta charset="UTF-8">\n<meta name="author" content="'"$author"'">\n<link rel="alternate" type="application/rss+xml" title="'"$blogdesc"'" href="'"$blogurl"'rss.xml">\n<meta name="viewport" content="width=device-width, initial-scale=1">\n</head>\n<body>\n<h1>'"$blogtitle"'</h1>\n<h3>'"$postdate"'</h3>\n<ul>\n</ul>\n</body>\n</html>' > $blogdir/index.html
 fi
 
@@ -29,7 +29,7 @@ then
   echo "Type your new entry, then hit return."
   read typed
 else
-  typed=`cat $1 | perl -pe "s/\n/ /"`
+  typed=$(cat $1 | perl -pe "s/\n/ /")
 fi
 
 # get all the relevant times and dates
@@ -42,18 +42,18 @@ function writeinfile {
 # will find the line with the most current date ... or rather check wether there was a post today
 # set line to be an integer so we can calculate later
 typeset -i line
-line=`grep -n -m 1 $postdate $workdir/index.html | cut -d: -f1`
+line=$(grep -n -m 1 $postdate $workdir/index.html | cut -d: -f1)
 
 # will find the line with the most current date ... or rather check wether there was a post today
 # set line to be an integer so we can calculate later
 typeset -i line
-line=`grep -n -m 1 $postdate $workdir/index.html | cut -d: -f1`
+line=$(grep -n -m 1 $postdate $workdir/index.html | cut -d: -f1)
 
 # check for postdate
 if [ "$line" -eq "0" ]
 then
   # find uppermost <h3>
-  line=`grep -n -m 1 "<h3>" $workdir/index.html | cut -d: -f1`
+  line=$(grep -n -m 1 "<h3>" $workdir/index.html | cut -d: -f1)
   # write new postdate plus entry above
   sed -i "$line i\<h3>$postdate</h3>\n<ul>\n<li><p>[<a href="$archpath$permalink.html">$time</a>] $typed</p></li>\n</ul>" $workdir/index.html
 else
@@ -69,7 +69,7 @@ writeinfile
 
 
 # write into monthly archive
-monarch=`date --utc +%Y%m`
+monarch=$(date --utc +%Y%m)
 mkdir -p $archdir/$monarch
 if [ ! -f $archdir/$monarch/index.html ]
 then
@@ -81,15 +81,15 @@ workdir=$archdir/$monarch
 writeinfile
 
 
-link=`date --utc +%B\ %Y`
+link=$(date --utc +%B\ %Y)
 if [ ! -f $archdir/index.html ]
 then
 echo -e '<!DOCTYPE html>\n<html lang='"$bloglang"'>\n<head>\n<title>Archive of '"$blogtitle"'</title>\n<meta charset="UTF-8">\n<meta name="author" content="'"$author"'">\n<link rel="alternate" type="application/rss+xml" title="'"$blogdesc"'" href="'"$blogurl"'rss.xml">\n<meta name="viewport" content="width=device-width, initial-scale=1">\n</head>\n<body>\n<h1>'"$blogtitle"'</h1>\n<p><a href="..">blog</a></p>\n<p><a href='"$monarch"'>'"$link"'</a></p>\n</body>\n</html>' > $archdir/index.html
 fi
-archline=`grep -n -m 1 $(date --utc +%Y%m) $archdir/index.html | cut -d: -f1`
+archline=$(grep -n -m 1 $(date --utc +%Y%m) $archdir/index.html | cut -d: -f1)
 if [ -z "$archline" ]
 then
-  archline=`grep -n -m 1 '<p><a' $archdir/index.html | cut -d: -f1`
+  archline=$(grep -n -m 1 '<p><a' $archdir/index.html | cut -d: -f1)
  # link=`date --utc +%B\ %Y`
   sed -i "$archline i\<p><a href=\"$monarch\">$link</a></p>" $archdir/index.html
 fi
@@ -102,8 +102,8 @@ echo -e '<!DOCTYPE html>\n<html lang='"$bloglang"'>\n<head>\n<title>'"$blogtitle
 # delete oldest entries
 if [ $(grep h3 $blogdir/index.html | wc -l) -gt $delblog ]
 then
-  h3del=`grep -n "h3" $blogdir/index.html | tail -1 | cut -d: -f 1`
-  uldel=`grep -n "/ul" $blogdir/index.html | tail -1 | cut -d: -f 1`
+  h3del=$(grep -n "h3" $blogdir/index.html | tail -1 | cut -d: -f 1)
+  uldel=$(grep -n "/ul" $blogdir/index.html | tail -1 | cut -d: -f 1)
   sed -i -e "$h3del,$(echo $uldel)d" $blogdir/index.html
 fi
 
@@ -115,9 +115,9 @@ function rss {
     length=$(echo $typed | wc -w)
     if [ "$length" -gt "5" ]
     then
-      title="`echo $typed | sed -e 's/<[^>]*>//g' | cut -d' ' -f-5` ..."
+      title="$(echo $typed | sed -e 's/<[^>]*>//g' | cut -d' ' -f-5) ..."
     else
-      title="`echo $typed | sed -e 's/<[^>]*>//g'`"
+      title="$(echo $typed | sed -e 's/<[^>]*>//g')"
     fi
 
     link=''"$blogurl"'archive/'"$permalink"'.html'
@@ -125,12 +125,12 @@ function rss {
     rssl='<item>\n<title>'"$title"'</title>\n<link>'"$link"'</link>\n<guid>'"$link"'</guid>\n<description><![CDATA[<p>'"$typed"'</p>]]></description>\n</item>'
 
     # write the new item to the rss file
-    rline=`grep -n -m 1 '<item>' $blogdir/rss.xml | cut -d: -f1`
+    rline=$(grep -n -m 1 '<item>' $blogdir/rss.xml | cut -d: -f1)
     sed -i "$rline i\ $rssl" $blogdir/rss.xml
 
 
     # remove the oldest entries in the rss feed
-    totrss=`grep -n "</item>" $blogdir/rss.xml | wc -l`
+    totrss=$(grep -n "</item>" $blogdir/rss.xml | wc -l)
     if [ $totrss -gt $delrss ]
     then
       gr=$(wc -l $blogdir/rss.xml | cut -d ' ' -f1)
