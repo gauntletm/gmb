2eba0937fa17b94f3732a950a238fd7759776ae6
Gauntlet O. Manatee <spukspital@openmailbox.org>
2018-07-23 19:00:29 +0200

    using mktemp
    using mktemp instead of fixed filename for temporary file

--- gpe.sh
+++ gpe.sh
@@ -1,36 +1,33 @@
 #!/bin/bash
 . ~/.config/gmb/gmbrc
 
-mkdir -p ~/tmp
-# better use mktemp
-
 # defining files
-pedfile=~/tmp/ped
+tempfile=$(mktemp)
 blogpost=$archdir/$1
 archiveindex=$archdir/$(echo $1 | head -c 6)/index.html
 rss=$blogdir/rss.xml
 blogindex=$blogdir/index.html
 
-grep -m 1 "<p>" $archdir/$1 > $pedfile
+grep -m 1 "<p>" $archdir/$1 > $tempfile
 
-$editor $pedfile
-ped=]$(cat $pedfile | cut -d] -f 2)
+$editor $tempfile
+templine=]$(cat $tempfile | cut -d] -f 2)
 
 # using } as delimiter because / is common in html and } is not
-sed -i "$(grep -n $1 $blogpost | cut -d: -f 1)s}].*$}$ped}" $blogpost
-sed -i "$(grep -n $1 $archiveindex | cut -d: -f 1)s}].*$}$ped}" $archiveindex
+sed -i "$(grep -n $1 $blogpost | cut -d: -f 1)s}].*$}$templine}" $blogpost
+sed -i "$(grep -n $1 $archiveindex | cut -d: -f 1)s}].*$}$templine}" $archiveindex
 
 # replace if still on index page
 if $(grep -q $1 $blogindex)
 then
-  sed -i "$(grep -n $1 $blogindex | cut -d: -f 1)s}].*$}$ped}" $blogindex
+  sed -i "$(grep -n $1 $blogindex | cut -d: -f 1)s}].*$}$templine}" $blogindex
 fi
 
 # replace if still in rss feed
 if $(grep -q $1 $rss)
 then
-ped=$(echo $ped | sed 's}] }<p>}' | sed 's}</li>}}')
-sed -i "$[ $(grep -n "$1</guid>" $rss | cut -d: -f 1) +1 ]s}\[CDATA\[.*\]\]}\[CDATA\[$ped\]\]}" $rss
+templine=$(echo $templine | sed 's}] }<p>}' | sed 's}</li>}}')
+sed -i "$[ $(grep -n "$1</guid>" $rss | cut -d: -f 1) +1 ]s}\[CDATA\[.*\]\]}\[CDATA\[$templine\]\]}" $rss
 fi
 
-rm ~/tmp/ped
+rm $tempfile
